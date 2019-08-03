12750aa773de9b116922c52d803179b1a7bb3fe1
Jean-Didier Garaud <jean-didier.garaud@onera.fr>
2019-08-03 18:56:32 +0200

    Also display alternate shortcuts

--- QShortcutEditor.py
+++ QShortcutEditor.py
@@ -1,7 +1,7 @@
 """
-QShortcutEditor. 
+QShortcutViewer and QShortcutEditor. 
 
-A PySide2 widget to display and configure an application's shortcuts.
+PySide2 widgets to display and configure an application's shortcuts.
 Can be tested as a standalone program (although it only has one shortcut).
 
 
@@ -22,30 +22,37 @@ Typical usage, call it from your QMainWindow::
 from PySide2 import QtCore, QtGui, QtWidgets
 
 
-class QShortcutEditor(QtWidgets.QWidget):
+class QShortcutViewer(QtWidgets.QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.resize(400,600)
         self.verticalLayout = QtWidgets.QVBoxLayout(self)
-        self.table = QtWidgets.QTableWidget(5, 2, self)
+        self.table = QtWidgets.QTableWidget(5, 3, self)
         self.verticalLayout.addWidget(self.table)
 
         self.table.horizontalHeader().setStretchLastSection(True)
         self.table.horizontalHeader().setVisible(False)
         self.table.horizontalHeader().setVisible(True)
-        self.table.setHorizontalHeaderLabels(['Shortcut', 'Action'])
+        self.table.setHorizontalHeaderLabels(['Shortcut', 'Alternate', 'Action'])
         self.table.verticalHeader().setVisible(False)
         
-        self.setWindowTitle("Customize shortcuts")
-#        self.setWindowIcon(pkgIcon('escale.png'))
+        self.setWindowTitle("Shortcuts")
+
         act = QtWidgets.QAction("Close", self)
         act.setShortcuts([QtGui.QKeySequence(QtCore.Qt.Key_Escape)])
         act.triggered.connect(self.close)
         self.addAction(act)
 
+        # could use QTableWidget.sortItems to allow sorting?
 
     def prepare(self, gui):
         "List all shortcuts from the given widget, to be ready for a show()."
+        # just a little cosmetics:
+        try:
+            self.setWindowIcon(gui.windowIcon())
+        except:
+            pass
+
         # ignore actions that don't have a shortcut
         actions = [ action for action in gui.findChildren(QtWidgets.QAction)
                     if (len(action.shortcuts()) > 0) ]
@@ -58,15 +65,25 @@ class QShortcutEditor(QtWidgets.QWidget):
         for (irow, action) in enumerate(actions):
             tip = action.toolTip()
             short = action.shortcuts()[0].toString()
+            alt = [ a.toString() for a in action.shortcuts()[1:] ]
             x = QtWidgets.QTableWidgetItem()
             x.setText(short)
             tab.setItem(irow, 0, x)
     
             x = QtWidgets.QTableWidgetItem()
-            x.setText(tip)
+            x.setText(' '.join(alt))
             tab.setItem(irow, 1, x)
             
-
+            x = QtWidgets.QTableWidgetItem()
+            x.setText(tip)
+            tab.setItem(irow, 2, x)
+            
+class QShortcutEditor(QShortcutViewer):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Customize shortcuts")
+        
+            
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
